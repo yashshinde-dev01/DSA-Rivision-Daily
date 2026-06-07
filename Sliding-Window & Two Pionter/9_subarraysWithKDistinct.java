4095a13f2f565ff4aca79a18bc6d97c358452756
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-06-07 20:11:52 +0530

    Add files via upload

---
+++ Sliding-Window & Two Pionter/9_subarraysWithKDistinct.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return Atmost(nums,k)-Atmost(nums,k-1);
+    }
+    private static int Atmost(int[]nums,int k){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int l=0;
+        int r=0;
+        int cnt=0;
+        while(r<nums.length){
+            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
+            while(map.size()>k){
+                map.put(nums[l], map.get(nums[l]) - 1);
+                if(map.get(nums[l])==0){
+                    map.remove(nums[l]);
+                }
+                l++;
+            }
+            cnt+=(r-l+1);
+            r++;
+        }
+        return cnt;
+    }
+}
+// hashmap + sliding window 
+//https://leetcode.com/problems/subarrays-with-k-different-integers/
