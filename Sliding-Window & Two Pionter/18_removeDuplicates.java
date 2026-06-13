4e031bc5c5d72bda5ecd23e564b1ca7054ee0300
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-06-13 12:53:13 +0530

    Add files via upload

---
+++ Sliding-Window & Two Pionter/18_removeDuplicates.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        int k = 1; 
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
+// two pointer--- slow and fast pointer 
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
