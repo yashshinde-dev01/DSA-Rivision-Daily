e44fcc2ef135d384fec43300a7ba4d2f173bc98a
yashshinde-dev01 <shindeyash.dev@gmail.com>
2026-06-21 22:00:16 +0530

    Add files via upload

---
+++ Kadane's Algo/2_ maxProduct.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int prefix=1;
+        int suffix=1;
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(prefix==0)prefix=1;
+            if(suffix==0)suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-1-i];
+            ans=Math.max(ans,Math.max(prefix,suffix));
+        }
+        return ans;
+
+    }
+}
+// kaden's algo
+//https://leetcode.com/problems/maximum-product-subarray/
